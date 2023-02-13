ff34e58c3024e6cc02569860dd4df021ebd0206d
antirandominette <choletl@protonmail.com>
2023-02-13 17:27:27 +0100

    IN-PROGRESS: Like dislike dislike feature is currently working but getting status 401 on first request

--- front/src/Pages/Forum/Forum.jsx
+++ front/src/Pages/Forum/Forum.jsx
@@ -7,8 +7,10 @@ import "./Forum.css";
 function Forum() {
     const navigate = useNavigate();
     const userToken = sessionStorage.getItem('token');
+    const userId = sessionStorage.getItem('userId');
     const userIsConnected = sessionStorage.getItem('isConnected');
     const [posts, setPosts] = useState([]);
+    const [vote, setVote] = useState(0);
 
     /* A hook that is called when the component is mounted. Fetches all Posts in database */
     useEffect(() => {
@@ -17,6 +19,7 @@ function Forum() {
         }
         if (userIsConnected && userToken) {
             fetch('http://13.37.164.181:4200/api/posts/', {
+                credentials: 'same-origin',
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -34,13 +37,64 @@ function Forum() {
                 })
                 .then(res => setPosts(res))
                 .catch(error => console.log(error));
-
         }
         // eslint-disable-next-line
     }, []);
 
+    async function handleVote(postId, data) {
+        await fetch(`http://13.37.164.181:4200/api/posts/${ postId }/like`, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + userToken,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => { return res.json(); })
+        .then(res => console.log(res))
+        .catch(error => console.log(error));
+    }
+
     function handleCreatePost() {
         navigate('/forum/create-post');
+    }
+
+    function handleUpvote(postId) {
+        console.log('like');
+
+        if (vote === 1) {
+            setVote(0);
+
+            const data = { userId: userId, like: 0 }
+            
+            handleVote(postId, data);
+        }
+        else if (vote === 0) {
+            setVote(1);
+            
+            const data = { userId: userId, like: 1 }
+
+            handleVote(postId, data);
+        }
+    }
+
+    function handleDownvote(postId) {
+        console.log('dislike');
+
+        if(vote === -1) {
+            setVote(0);
+            
+            const data = { userId: userId, like: vote }
+    
+            handleVote(postId, data);
+        }
+        else if (vote === 0) {
+            setVote(-1);
+
+            const data = { userId: userId, like: vote }
+            
+            handleVote(postId, data);
+        }
     }
 
     return (
@@ -64,11 +118,11 @@ function Forum() {
                                     </div>
 
                                     <div className="forum_post_votes_container">
-                                            <svg className="vote_arrow upvote" fill="#000000" width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
+                                            <svg onClick={ () => handleUpvote(post._id) } className="vote_arrow upvote" fill="#000000" width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
                                             {
                                                 <p className="forum_post_votes">{ post.likes - post.dislikes }</p>
                                             }
-                                            <svg className="vote_arrow downvote" fill="#000000" width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
+                                            <svg onClick={ () => handleDownvote(post._id) } className="vote_arrow downvote" fill="#000000" width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
                                     </div>
                                 </div>
                             )
